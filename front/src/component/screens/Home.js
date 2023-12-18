efa042b37b711d1a38b4e7b82c469d48c414cb2b
binyamats <biatsb5656@gmail.com>
2023-12-18 22:54:31 +0300

    make changes on  the homepage about section and change picture

--- front/src/component/screens/Home.js
+++ front/src/component/screens/Home.js
@@ -17,12 +17,57 @@ export const Home = () => {
   </div>
   <div class="flex items-center lg:mr-4 lg:order-2">
     <button style={{ backgroundColor: "#DE8622", fontSize: "0.8em", width: "10em", borderRadius: "5px", marginRight: "1em" }} className="block mb-4 lg:mb-0">Read More</button>
-    <p style={{ color: "#454545", fontSize: "1em" }} className="pl-6">Hello world, this is not what it may be, it is what it is</p>
+    <p style={{ color: "#454545", fontSize: "1em" }} className="pl-6"> we aim to bring student's to back school </p>
   </div>
   <div class="flex min-h-screen items-center justify-center flex-col pt-8">
   <div class="relative flex w-full max-w-[56rem] flex-row rounded-xl bg-white bg-clip-border text-gray-700 shadow-md mb-8">
     <div class="relative m-0 w-2/5 shrink-0 overflow-hidden rounded-xl rounded-r-none bg-white bg-clip-border text-gray-700">
       <img
+        src="F:\website projects\Save_Tigray\front\public\images\damage_of_schools.jpg"
+        alt="image"
+        class="h-full w-full object-cover"
+      />
+    </div>
+    <div class="p-6">
+      
+      <h4 class="mb-2 block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+        Situation of education in Tigray before crisis
+      </h4>
+      <p class="mb-8 block font-sans text-base font-normal leading-relaxed text-gray-700 antialiased">
+        The genocidal war victimized students, supportive staffs and teachers.
+        Female teachers and students were victims of rape and brutal sexual 
+        violence. Manylost their lives. The invading forces also damaged school
+        buildings and infrastructure, looted supplies
+        <a class="inline-block" href="#">
+          <button
+            class="flex select-none items-center gap-2 rounded-lg py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-pink-500 transition-all hover:bg-pink-500/10 active:bg-pink-500/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+            type="button"
+          >
+            Learn More
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="currentColor"
+              aria-hidden="true"
+              class="h-4 w-4"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+              ></path>
+            </svg>
+          </button>
+        </a>
+      </p>  
+    </div>
+  </div>
+
+  <div class="relative flex w-full max-w-[56rem] flex-row rounded-xl bg-white bg-clip-border text-gray-700 shadow-md mb-8">
+    <div class="relative m-0 w-2/5 shrink-0 overflow-hidden rounded-xl rounded-r-none bg-white bg-clip-border text-gray-700">
+      <img
         src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1471&amp;q=80"
         alt="image"
         class="h-full w-full object-cover"
@@ -31,13 +76,14 @@ export const Home = () => {
     <div class="p-6">
       
       <h4 class="mb-2 block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
-        Lyft launching cross-platform service this week
+        HUMAN LOSS FROM THE EDUCATION SECTOR
       </h4>
       <p class="mb-8 block font-sans text-base font-normal leading-relaxed text-gray-700 antialiased">
-        Like so many organizations these days, Autodesk is a company in
-        transition. It was until recently a traditional boxed software company
-        selling licenses. Yet its own business model disruption is only part of
-        the story
+        Due to the conflict the education sector suffered from trauma, physical 
+        injuries,psychological disorder and looting of possessions. For the last
+        8 months no salary was paid and no access to banking service which worsen
+        the life of school community and impacted the effort of school reopening 
+        and the right of children to access quality education.
       </p>
       <a class="inline-block" href="#">
         <button
@@ -76,52 +122,7 @@ export const Home = () => {
     <div class="p-6">
       
       <h4 class="mb-2 block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
-        Lyft launching cross-platform service this week
-      </h4>
-      <p class="mb-8 block font-sans text-base font-normal leading-relaxed text-gray-700 antialiased">
-        Like so many organizations these days, Autodesk is a company in
-        transition. It was until recently a traditional boxed software company
-        selling licenses. Yet its own business model disruption is only part of
-        the story
-      </p>
-      <a class="inline-block" href="#">
-        <button
-          class="flex select-none items-center gap-2 rounded-lg py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-pink-500 transition-all hover:bg-pink-500/10 active:bg-pink-500/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-          type="button"
-        >
-          Learn More
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="2"
-            stroke="currentColor"
-            aria-hidden="true"
-            class="h-4 w-4"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-            ></path>
-          </svg>
-        </button>
-      </a>
-    </div>
-  </div>
-
-  <div class="relative flex w-full max-w-[56rem] flex-row rounded-xl bg-white bg-clip-border text-gray-700 shadow-md mb-8">
-    <div class="relative m-0 w-2/5 shrink-0 overflow-hidden rounded-xl rounded-r-none bg-white bg-clip-border text-gray-700">
-      <img
-        src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1471&amp;q=80"
-        alt="image"
-        class="h-full w-full object-cover"
-      />
-    </div>
-    <div class="p-6">
-      
-      <h4 class="mb-2 block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
-        Lyft launching cross-platform service this week
+        DAMAGE OF BUILDINGS AND SUPPLIES
       </h4>
       <p class="mb-8 block font-sans text-base font-normal leading-relaxed text-gray-700 antialiased">
         Like so many organizations these days, Autodesk is a company in
